0e5eb2197f397d32f5c27d6f215521159dca7bd2
coji <coji@techtalk.jp>
2025-01-05 01:16:55 +0900

    feat: add generation logs on turso database

--- app/routes/api.ts
+++ app/routes/api.ts
@@ -1,6 +1,9 @@
 import { google } from '@ai-sdk/google'
+import { createId } from '@paralleldrive/cuid2'
 import { streamObject } from 'ai'
+import { sql } from 'kysely'
 import { z } from 'zod'
+import { db } from '~/services/db.server'
 import type { Route } from './+types/api'
 
 export const inputSchema = z.object({
@@ -23,25 +26,56 @@ export const outputSchema = z.object({
 export const action = async ({ request }: Route.ActionArgs) => {
   const json = await request.json()
   const submission = inputSchema.parse(json)
+  const prompt = `あなたは気鋭の作家です。
 
-  const result = await streamObject({
-    model: google('gemini-2.0-flash-exp'),
-    prompt: `あなたは気鋭の作家です。
-    
-${submission.productCategory}が登場しつつ、
-${submission.brandImages.join(', ')}というイメージで、短編小説を執筆してください。
+${submission.brandImages.join(', ')}というイメージで、
+${submission.productCategory}が登場する、
+短編小説を執筆してください。
 
 主人公は${submission.targetUserImage}です。
 
 ストーリーを生成したあと、以下を生成してください。
+
 1. タイトル
 2. 主人公の最後の言葉
 3. ストーリーにインスパイアされた、句点で区切られた3行短詩
 
 短編小説は本文のみで Markdown 形式で段落ごとに分けて記述してください。
-`,
+`
+  const modelName: string = 'gemini-2.0-flash-exp'
+  const temperature: number = 0.5
+
+  const result = await streamObject({
+    model: google('gemini-2.0-flash-exp'),
+    prompt,
     schema: outputSchema,
-    temperature: 0.5,
+    temperature,
+    onFinish: async (event) => {
+      await db
+        .insertInto('generationLogs')
+        .values({
+          id: createId(),
+          provider: 'google',
+          modelName: modelName,
+          temperature,
+          prompt,
+          brandImages: JSON.stringify(submission.brandImages),
+          productName: submission.productName,
+          productCategory: submission.productCategory,
+          targetUserImage: submission.targetUserImage,
+          story: event.object?.novel ?? null,
+          candidates: JSON.stringify([
+            event.object?.title,
+            event.object?.theProtagonistsLastWords,
+            ...(event.object?.shortPoemsInspiredByTheStory?.split('。') ?? []),
+          ]),
+          usagePromptTokens: event.usage.promptTokens,
+          usageCompletionTokens: event.usage.completionTokens,
+          usageTotalTokens: event.usage.totalTokens,
+          createdAt: sql`CURRENT_TIMESTAMP`,
+        })
+        .execute()
+    },
   })
 
   return result.toTextStreamResponse()
--- app/services/db.server.ts
+++ app/services/db.server.ts
@@ -1,9 +1,15 @@
-import { createClient } from '@libsql/client'
-import { drizzle } from 'drizzle-orm/libsql'
+import { createClient } from '@libsql/client/web'
+import { LibsqlDialect } from '@libsql/kysely-libsql'
+import { CamelCasePlugin, Kysely } from 'kysely'
+import type { DB } from './types'
 
-const turso = createClient({
-  url: process.env.TURSO_DATABASE_URL!,
-  authToken: process.env.TURSO_AUTH_TOKEN,
+export const db = new Kysely<DB>({
+  dialect: new LibsqlDialect({
+    client: createClient({
+      url: process.env.DATABASE_URL!,
+      authToken: process.env.TURSO_AUTH_TOKEN,
+      // biome-ignore lint/suspicious/noExplicitAny: <explanation>
+    }) as any,
+  }),
+  plugins: [new CamelCasePlugin()],
 })
-
-export const db = drizzle(turso)
