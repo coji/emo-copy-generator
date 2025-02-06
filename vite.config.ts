512d114bff21d0d3f3d2235ff89c00e17cbe9dfe
coji <coji@techtalk.jp>
2025-02-06 22:25:19 +0900

    refactor: remove unused Vercel configuration files and update build scripts

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,11 @@ import tailwindcss from 'tailwindcss'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-export default defineConfig(({ isSsrBuild, command }) => ({
+export default defineConfig({
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
     },
   },
-  build: {
-    rollupOptions: isSsrBuild ? { input: './server/app.ts' } : undefined,
-  },
-  ssr: { noExternal: command === 'build' ? true : undefined },
   plugins: [reactRouter(), tsconfigPaths()],
-}))
+})
